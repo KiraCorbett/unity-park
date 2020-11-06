5e0a8dcfbc4d103b90a30939e49a804063fc2ce3
Kira Corbett <kiracorbett00@gmail.com>
2020-11-06 09:53:48 -0800

    @Carson-Key init login

--- myApp/src/pages/Tab1.tsx
+++ myApp/src/pages/Tab1.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonRow, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+    <IonGrid>
+      <IonRow class="ion-justify-content-center">
+        <form className="ion-padding">
+          <IonItem>
+            <IonLabel position="floating">Username</IonLabel>
+            <IonInput />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput type="password" />
+          </IonItem>
+          <IonItem lines="none">
+            <IonLabel>Remember me</IonLabel>
+            <IonCheckbox defaultChecked={true} slot="start" />
+          </IonItem>
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            Login
+          </IonButton>
+        </form>
+      </IonRow>
+    </IonGrid>
   );
 };
 
